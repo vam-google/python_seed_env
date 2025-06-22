35d1eef7f5b485f8cc2731c295547b1a763418af
Kanglan Tang <kanglan@google.com>
2025-06-22 20:24:11 +0000

    Test with the prototype with patched lock file on python 3.10 gpu vm

--- build_seed_env.sh
+++ build_seed_env.sh
@@ -4,7 +4,7 @@ set -e
 
 # Picking latest (for the moment of writing this) released JAX and latest
 # MaxText commit
-curl https://raw.githubusercontent.com/jax-ml/jax/c09b1bb763d846a694f919e5a5adda9575ce66d6/build/requirements_lock_3_10.txt -O
+curl https://raw.githubusercontent.com/jax-ml/jax/1ad05bb26105f23ee7728b36cca12901fe70e187/build/requirements_lock_3_10.txt -O # jax-v0.6.2
 curl https://raw.githubusercontent.com/AI-Hypercomputer/maxtext/80c0884a413219bbc317eaea20571d995cbe6566/requirements.txt -O
 
 # Fixing current maxtext requirements.txt
@@ -17,6 +17,7 @@ sed -i 's/sentencepiece==0.1.97/sentencepiece>=0.1.97/g' requirements.txt
 sed -i 's/\/JetStream.git/\/JetStream.git@261f25007e4d12bb57cf8d5d61e291ba8f18430f/g' requirements.txt
 sed -i 's/\/logging.git/\/logging.git@44b4810e65e8c0a7d9e4e207c60e51d9458a3fb8/g' requirements.txt
 echo "tensorflow-metadata>=1.14.0" >> requirements.txt
+patch requirements_lock_3_10.txt < jax_requirements_lock_3_10.patch
 
 # Commands to create env
 # ---------------------------------
@@ -35,12 +36,12 @@ uv lock --managed-python --resolution=lowest
 uv export --managed-python --locked --no-hashes --no-annotate --resolution=lowest --output-file=maxtext_requirements_lock_3_10.txt
 
 # Congrats, you've done it:
-#   - maxtext_requirements_lock_3_12.txt is your well-defined reproducible python
+#   - maxtext_requirements_lock_3_10.txt is your well-defined reproducible python
 #     environment to install in a virtual env or a Docker image
 #   - pyproject.toml - commit this into your github repo, using it one will
 #     always be able to reproduce the environment from maxtext commit by running:
 #       uv lock --managed-python --resolution=lowest # recreates uv.lock
-#       uv export --managed-python --locked --no-hashes --no-annotate --resolution=lowest --output-file=maxtext_requirements_lock_3_12.txt # regenerates lock.txt
+#       uv export --managed-python --locked --no-hashes --no-annotate --resolution=lowest --output-file=maxtext_requirements_lock_3_10.txt # regenerates lock.txt
 
 
 # Commands to validate and inspect created env
