1ccf7751b473679c95232511f870922d86f237b9
Kanglan Tang <kanglan@google.com>
2025-06-22 20:33:41 +0000

    Test with the prototype on python 3.11 gpu vm

--- build_seed_env.sh
+++ build_seed_env.sh
@@ -4,7 +4,7 @@ set -e
 
 # Picking latest (for the moment of writing this) released JAX and latest
 # MaxText commit
-curl https://raw.githubusercontent.com/jax-ml/jax/c09b1bb763d846a694f919e5a5adda9575ce66d6/build/requirements_lock_3_12.txt -O
+curl https://raw.githubusercontent.com/jax-ml/jax/d3f08713bc8cc2700851c61c55d7d7dde1de5a02/build/requirements_lock_3_11.txt -O
 curl https://raw.githubusercontent.com/AI-Hypercomputer/maxtext/80c0884a413219bbc317eaea20571d995cbe6566/requirements.txt -O
 
 # Fixing current maxtext requirements.txt
@@ -19,7 +19,7 @@ sed -i 's/\/logging.git/\/logging.git@44b4810e65e8c0a7d9e4e207c60e51d9458a3fb8/g
 
 # Commands to create env
 # ---------------------------------
-uv add --managed-python --no-build --no-sync --resolution=highest -r requirements_lock_3_12.txt
+uv add --managed-python --no-build --no-sync --resolution=highest -r requirements_lock_3_11.txt
 # Uncomment if building GPU env
 # cat constraints_gpu_only.txt | xargs -I {} uv remove --managed-python --no-sync --resolution=highest {}
 # Comment if building TPU env
@@ -27,19 +27,19 @@ cat constraints_tpu_only.txt | xargs -I {} uv remove --managed-python --no-sync 
 # Here if there are any deps in project.toml that conflict with
 # maxtext_requirements.txt, lower bound them  in project.toml manually
 uv add --managed-python --no-sync --resolution=highest -r requirements.txt
-uv export --managed-python --locked --no-hashes --no-annotate --resolution=highest --output-file=maxtext_requirements_lock_3_12.txt
-python3 lock_to_lower_bound_project.py maxtext_requirements_lock_3_12.txt pyproject.toml
+uv export --managed-python --locked --no-hashes --no-annotate --resolution=highest --output-file=maxtext_requirements_lock_3_11.txt
+python3 lock_to_lower_bound_project.py maxtext_requirements_lock_3_11.txt pyproject.toml
 rm uv.lock
 uv lock --managed-python --resolution=lowest
-uv export --managed-python --locked --no-hashes --no-annotate --resolution=lowest --output-file=maxtext_requirements_lock_3_12.txt
+uv export --managed-python --locked --no-hashes --no-annotate --resolution=lowest --output-file=maxtext_requirements_lock_3_11.txt
 
 # Congrats, you've done it:
-#   - maxtext_requirements_lock_3_12.txt is your well-defined reproducible python
+#   - maxtext_requirements_lock_3_11.txt is your well-defined reproducible python
 #     environment to install in a virtual env or a Docker image
 #   - pyproject.toml - commit this into your github repo, using it one will
 #     always be able to reproduce the environment from maxtext commit by running:
 #       uv lock --managed-python --resolution=lowest # recreates uv.lock
-#       uv export --managed-python --locked --no-hashes --no-annotate --resolution=lowest --output-file=maxtext_requirements_lock_3_12.txt # regenerates lock.txt
+#       uv export --managed-python --locked --no-hashes --no-annotate --resolution=lowest --output-file=maxtext_requirements_lock_3_11.txt # regenerates lock.txt
 
 
 # Commands to validate and inspect created env
